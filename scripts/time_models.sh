ff5ebadfdace022e06f93dc5d9fac0a164a1681d
dansola <soladbs@gmail.com>
2021-08-06 15:19:29 -0400

    fixed lbc unet, got timing results

--- scripts/time_models.sh
+++ scripts/time_models.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 cd ..
 cd src/train
+
+python train_timer.py --epochs 3 --device cuda --model unet
+python train_timer.py --epochs 3 --device cuda --model lbc_unet
+python train_timer.py --epochs 3 --device cuda --model small_axial_lbc_unet
+python train_timer.py --epochs 3 --device cuda --model deeplab_mobile_net
+python train_timer.py --epochs 3 --device cuda --model lraspp_mobile_net
+python train_timer.py --epochs 3 --device cuda --model dsc_unet
+python train_timer.py --epochs 3 --device cuda --model dsc_lbc_unet
+python train_timer.py --epochs 3 --device cuda --model small_dsc_lbc_unet
+
+python train_timer.py --epochs 3 --device cpu --model unet
+python train_timer.py --epochs 3 --device cpu --model lbc_unet
+python train_timer.py --epochs 3 --device cpu --model small_axial_lbc_unet
+python train_timer.py --epochs 3 --device cpu --model deeplab_mobile_net
+python train_timer.py --epochs 3 --device cpu --model lraspp_mobile_net
+python train_timer.py --epochs 3 --device cpu --model dsc_unet
+python train_timer.py --epochs 3 --device cpu --model dsc_lbc_unet
+python train_timer.py --epochs 3 --device cpu --model small_dsc_lbc_unet
+
+
 #python train_timer.py --epochs 10 --device cuda --model unet
 #python train_timer.py --epochs 10 --device cuda --model small_unet
 #python train_timer.py --epochs 10 --device cuda --model axial_unet
@@ -11,12 +31,12 @@ cd src/train
 #python train_timer.py --epochs 10 --device cuda --model small_axial_lbc_unet
 #python train_timer.py --epochs 10 --device cuda --model small_axial_lbc_unet_10
 
-python train_timer.py --epochs 10 --device cpu --model unet
-python train_timer.py --epochs 10 --device cpu --model small_unet
-python train_timer.py --epochs 10 --device cpu --model axial_unet
-python train_timer.py --epochs 10 --device cpu --model small_axial_unet
-python train_timer.py --epochs 10 --device cpu --model lbc_unet
-python train_timer.py --epochs 10 --device cpu --model small_lbc_unet
-python train_timer.py --epochs 10 --device cpu --model axial_lbc_unet
-python train_timer.py --epochs 10 --device cpu --model small_axial_lbc_unet
-python train_timer.py --epochs 10 --device cpu --model small_axial_lbc_unet_10
+#python train_timer.py --epochs 10 --device cpu --model unet
+#python train_timer.py --epochs 10 --device cpu --model small_unet
+#python train_timer.py --epochs 10 --device cpu --model axial_unet
+#python train_timer.py --epochs 10 --device cpu --model small_axial_unet
+#python train_timer.py --epochs 10 --device cpu --model lbc_unet
+#python train_timer.py --epochs 10 --device cpu --model small_lbc_unet
+#python train_timer.py --epochs 10 --device cpu --model axial_lbc_unet
+#python train_timer.py --epochs 10 --device cpu --model small_axial_lbc_unet
+#python train_timer.py --epochs 10 --device cpu --model small_axial_lbc_unet_10
--- src/train/train_timer.py
+++ src/train/train_timer.py
@@ -2,10 +2,14 @@ import os
 import sys
 
 import time
+
+from src.models.dsc.dsc_lbc_unet import DSCUNetLBP, DSCSmallUNetLBP
+from src.models.dsc.dsc_unet import UNetDSC
 from src.models.lbcnn.axial_lbcnn import AxialUNetLBC, SmallAxialUNetLBC
 from src.models.lbcnn.axial_unet import AxialUNet, SmallAxialUNet
 from src.models.lbcnn.lbc_unet import UNetLBP, SmallUNetLBP
 from src.models.unet.unet_model import UNet, SmallUNet
+from torchvision.models.segmentation import deeplabv3_mobilenet_v3_large, lraspp_mobilenet_v3_large
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -34,7 +38,7 @@ def get_args():
                         help='Directory where images, masks, and txt files reside.', dest='data_dir')
     parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
                         help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=1,
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
                         help='Batch size', dest='batchsize')
     parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.0001,
                         help='Learning rate', dest='lr')
@@ -44,7 +48,7 @@ def get_args():
                         help='Downscaling factor of the images')
     parser.add_argument('-c', '--crop', dest='crop', type=int, default=256,
                         help='Height and width of images and masks.')
-    parser.add_argument('-m', '--model', dest='model', type=str, default='small_axial_lbc_unet_10',
+    parser.add_argument('-m', '--model', dest='model', type=str, default='deeplab_mobile_net',
                         help='Model to use.')
     parser.add_argument('-dev', '--device', dest='device', type=str, default='cuda',
                         help='Train on gpu vs cpu.')
@@ -55,16 +59,12 @@ def get_args():
 def train_net(net, data_dir, device, epochs=20, batch_size=1, lr=0.0001, save_cp=True, img_scale=0.35, img_crop=320):
     train_set = Ice(os.path.join(data_dir, 'imgs'), os.path.join(data_dir, 'masks'),
                     os.path.join(data_dir, 'txt_files'), 'train', img_scale, img_crop)
-    val_set = Ice(os.path.join(data_dir, 'imgs'), os.path.join(data_dir, 'masks'),
-                  os.path.join(data_dir, 'txt_files'), 'val', img_scale, img_crop)
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_set, batch_size=batch_size)
 
     global_step = 0
 
     optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min' if net.n_classes > 1 else 'max', patience=2)
     criterion = nn.CrossEntropyLoss()
 
     for epoch in range(epochs):
@@ -75,28 +75,17 @@ def train_net(net, data_dir, device, epochs=20, batch_size=1, lr=0.0001, save_cp
                 imgs = batch['image']
                 true_masks = batch['mask']
 
-                assert imgs.shape[1] == net.n_channels, \
-                    f'Network has been defined with {net.n_channels} input channels, ' \
-                    f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
-                    'the images are loaded correctly.'
-
                 imgs = imgs.to(device=device, dtype=torch.float32)
                 target = true_masks.to(device=device, dtype=torch.long)
 
-                masks_pred = net(imgs)
+                if 'mobile' in args.model:
+                    masks_pred = net(imgs)['out']
+                else:
+                    masks_pred = net(imgs)
+
                 probs = F.softmax(masks_pred, dim=1)
                 argmx = torch.argmax(probs, dim=1).to(dtype=torch.float32)
-
-                # example_images = [wandb.Image(imgs[0], caption='Image'),
-                #                   wandb.Image(target.to(dtype=torch.float)[0],
-                #                               caption='True Mask'),
-                #                   wandb.Image(argmx[0],
-                #                               caption='Predicted Mask')]
-
-                # wandb.log({"Examples": example_images})
-
                 loss = criterion(masks_pred, target.squeeze(1))
-                # wandb.log({"Training Loss": loss})
                 epoch_loss += loss.item()
 
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
@@ -108,16 +97,7 @@ def train_net(net, data_dir, device, epochs=20, batch_size=1, lr=0.0001, save_cp
 
                 pbar.update(imgs.shape[0])
                 global_step += 1
-                # if len(train_set) > 10:
-                #     n = 10
-                # else:
-                #     n = 1
-                # if global_step % (len(train_set) // (n * batch_size)) == 0:
-                #     val_loss, val_iou, val_acc = eval_net(net, val_loader, device)
-                    # wandb.log({"Validation Loss": val_loss})
-                    # wandb.log({"Validation IoU": val_iou})
-                    # wandb.log({"Validation Accuracy": val_acc})
-                    # scheduler.step(val_loss)
+
 
         if save_cp:
             try:
@@ -150,6 +130,16 @@ if __name__ == '__main__':
         net = SmallAxialUNetLBC(3, 3, 32)
     elif args.model == 'small_axial_lbc_unet_10':
         net = SmallAxialUNetLBC(3, 3, 10)
+    elif args.model == 'deeplab_mobile_net':
+        net = deeplabv3_mobilenet_v3_large(num_classes=3)
+    elif args.model == 'lraspp_mobile_net':
+        net = lraspp_mobilenet_v3_large(num_classes=3)
+    elif args.model == 'dsc_unet':
+        net = UNetDSC(n_channels=3, n_classes=3, bilinear=True)
+    elif args.model == 'dsc_lbc_unet':
+        net = DSCUNetLBP(3, 3)
+    elif args.model == 'small_dsc_lbc_unet':
+        net = DSCSmallUNetLBP(3, 3)
     else:
         raise ValueError('Please enter a valid model name.')
 
@@ -162,25 +152,42 @@ if __name__ == '__main__':
     net.to(device=device)
 
     try:
-        with torch.autograd.profiler.profile(use_cuda=True) as prof:
-            train_net(net=net, data_dir=args.data_dir, epochs=args.epochs, batch_size=args.batchsize, lr=args.lr,
-                      device=device,
-                      img_scale=args.scale, img_crop=args.crop)
-        print(prof)
+        st = time.time()
+        train_net(net=net, data_dir=args.data_dir, epochs=args.epochs, batch_size=args.batchsize, lr=args.lr,
+                  device=device,
+                  img_scale=args.scale, img_crop=args.crop)
+        run_time = time.time() - st
+
+        if os.path.exists(f'model_profile_{args.device}_v3.json'):
+            with open(f'model_profile_{args.device}_v3.json') as f:
+                data = json.load(f)
+            data[args.model] = run_time
+            with open(f'model_profile_{args.device}_v3.json', 'w') as outfile:
+                json.dump(data, outfile)
+        else:
+            data = {args.model: run_time}
+            with open(f'model_profile_{args.device}_v3.json', 'w') as outfile:
+                json.dump(data, outfile)
+
+        # with torch.autograd.profiler.profile(use_cuda=True) as prof:
+        #     train_net(net=net, data_dir=args.data_dir, epochs=args.epochs, batch_size=args.batchsize, lr=args.lr,
+        #               device=device,
+        #               img_scale=args.scale, img_crop=args.crop)
+        # # print(prof)
         # cpu_time = float(str(prof).split('\n')[-3].split(' ')[-1][:-1])
         # cuda_time = float(str(prof).split('\n')[-2].split(' ')[-1][:-1])
         #
         # print(f'CPU Time: {cpu_time}, Cuda Time: {cuda_time}')
-
-        # if os.path.exists(f'model_profile_{args.device}.json'):
-        #     with open(f'model_profile_{args.device}.json') as f:
+        #
+        # if os.path.exists(f'model_profile_{args.device}_v2.json'):
+        #     with open(f'model_profile_{args.device}_v2.json') as f:
         #         data = json.load(f)
         #     data[args.model] = {'cpu_time': cpu_time, 'cuda_time': cuda_time}
-        #     with open(f'model_profile_{args.device}.json', 'w') as outfile:
+        #     with open(f'model_profile_{args.device}_v2.json', 'w') as outfile:
         #         json.dump(data, outfile)
         # else:
         #     data = {args.model: {'cpu_time': cpu_time, 'cuda_time': cuda_time}}
-        #     with open(f'model_profile_{args.device}.json', 'w') as outfile:
+        #     with open(f'model_profile_{args.device}_v2.json', 'w') as outfile:
         #         json.dump(data, outfile)
 
     except KeyboardInterrupt:
